1482c2609b87c7589f73d07678f1241d2ede7777
amosISA <amosisa700@gmail.com>
2018-05-06 19:04:54 +0200

    Added some styles to login

--- config/settings/base.py
+++ config/settings/base.py
@@ -139,6 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "subvenciones/static"),
     os.path.join(BASE_DIR, "profiles/static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 MEDIA_URL = '/media/'

db0e267f63c5250f8a6dad08e5a6f62ab9c0f9e0
amosISA <amosisa700@gmail.com>
2018-06-07 20:44:52 +0200

    New styles to 404 and 500 errors

---
+++ static/home/js/dots_background.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    function freshDot(){
+        this.obj = document.createElement("div");
+        this.obj.classList.add("box");
+        this.obj.style.top = (window.innerHeight * Math.random()) + 'px';
+        this.obj.style.left = (window.innerWidth * Math.random()) + 'px';
+        this.size = 2;
+        this.obj.style.height =  this.size + 'px';
+        this.obj.style.width = this.size + 'px';
+
+        document.body.appendChild(this.obj);
+    }
+
+    var dot = [];
+    for(var i = 0 ; i < 200 ; i++ ){
+        dot.push(new freshDot());
+    }
+});

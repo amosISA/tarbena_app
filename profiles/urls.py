f0bdd94709cc9af4b13f786ab2039b035371ecb3
amosISA <amosisa700@gmail.com>
2018-06-19 13:58:24 +0200

    Added styles to profile - remain backend

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import ProfileDetailView, upload_avatar
+from .views import ProfileDetailView, upload_avatar , ProfileUpdateView
 
 urlpatterns = [
     url(r'^(?P<username>[\w-]+)/$', ProfileDetailView.as_view(), name='user_profile'),
     url(r'^upload/avatar/$', upload_avatar, name='upload_avatar'),
+    url(r'^configuration/', ProfileUpdateView.as_view(), name='update_profile'),
 ]

d2960f705b8f73200b984cc549d7ee2bc44cebd1
amosISA <amosisa700@gmail.com>
2018-08-19 13:00:45 +0000

    Added new wsgi modified file

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings 
 
+sys.path.append('/home/admin/tarbena/src/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+# prevent UnicodeEncodeError
+os.environ.setdefault('LANG', 'en_US.UTF-8')
+os.environ.setdefault('LC_ALL', 'en_US.UTF-8')
+
+if settings.DEBUG == False:
+    # activamos nuestro virtualenv
+    activate_this = '/home/admin/tarbena/bin/activate_this.py'
+    exec(open(activate_this).read())
 
 application = get_wsgi_application()

12c63c84f204b58438855819f0243a02cc34089f
amosISA <amosisa700@gmail.com>
2018-10-08 10:47:27 +0200

    Added new fields to contracts

--- contracts/admin.py
+++ contracts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf.locale.es import formats
 
-from .models import Contract, TypeContract, Contractor
+from .models import Contract, TypeContract, Contractor, AplicacionPresupuestaria, Organos
 
 formats.DATE_FORMAT = "d/m/Y"
 
@@ -40,3 +40,24 @@ class ContractorAdmin(admin.ModelAdmin):
     list_display_links = ('name', 'dni',)
     show_full_result_count = True
 admin.site.register(Contractor, ContractorAdmin)
+
+class AplicacionPresupuestariaAdmin(admin.ModelAdmin):
+    list_display = ['ejercicio', 'aplicacion_presupuestaria', 'importe', 'importe_recursos',
+                    'valor_estimado', 'porcentaje', 'organo']
+    list_filter = ['ejercicio', 'aplicacion_presupuestaria', 'importe', 'importe_recursos',
+                    'valor_estimado', 'porcentaje', 'organo']
+    search_fields = ['ejercicio', 'aplicacion_presupuestaria', 'importe', 'importe_recursos',
+                    'valor_estimado', 'porcentaje', 'organo']
+    empty_value_display = '-'
+    list_display_links = ('aplicacion_presupuestaria',)
+    show_full_result_count = True
+admin.site.register(AplicacionPresupuestaria, AplicacionPresupuestariaAdmin)
+
+class OrganosAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+    search_fields = ['name']
+    empty_value_display = '-'
+    list_display_links = ('name',)
+    show_full_result_count = True
+admin.site.register(Organos, OrganosAdmin)
--- contracts/models.py
+++ contracts/models.py
@@ -32,7 +32,34 @@ class Contractor(TimeStampedModel):
     class Meta:
         ordering = ["name"]
 
+class Organos(TimeStampedModel):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = 'Organos'
+        verbose_name_plural = "Organos"
+
+class AplicacionPresupuestaria(TimeStampedModel):
+    ejercicio = models.CharField(max_length=250, blank=True, null=True)
+    aplicacion_presupuestaria = models.CharField(max_length=250, blank=True, null=True)
+    importe = models.CharField(max_length=250, blank=True, null=True)
+    importe_recursos = models.CharField(max_length=250, blank=True, null=True, help_text='Importe de los recursos del presupuesto')
+    valor_estimado = models.CharField(max_length=250, blank=True, null=True, help_text='Valor estimado del contrato')
+    porcentaje = models.CharField(max_length=250, blank=True, null=True, help_text='% sobre los recursos')
+    organo = models.ForeignKey(Organos, related_name='aplic_presup_organo', blank=True, null=True)
+
+    def __str__(self):
+        return '{}'.format(self.aplicacion_presupuestaria)
+
+    class Meta:
+        ordering = ["aplicacion_presupuestaria"]
+
 class Contract(TimeStampedModel):
+    identificador = models.CharField(max_length=250, blank=True, null=True)
     type = models.ForeignKey(TypeContract, related_name='type_contract', blank=True, null=True)
     contractor = models.ForeignKey(Contractor, related_name='contract_contractor', blank=True, null=True)
     base = models.CharField(max_length=250, blank=True, null=True)
@@ -41,8 +68,8 @@ class Contract(TimeStampedModel):
     date_contract = models.DateField(blank=True, null=True)
     cpv = models.CharField(max_length=250, blank=True, null=True)
     duracion = models.CharField(max_length=250, blank=True, null=True)
-    objeto = models.TextField(blank=True)
-    aplicacion_presupuestaria = models.TextField(blank=True)
+    objeto = models.TextField(blank=True, help_text='Necesidad a satisfacer / Objeto del contrato')
+    aplic_presupuestaria = models.ForeignKey(AplicacionPresupuestaria, related_name='contract_aplicacion_presupuestaria', default='', blank=True, null=True)
     description = models.TextField(blank=True)
 
     class Meta:

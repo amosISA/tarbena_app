caf4930ce29005b5e14757dc132fc895bc0f4d14
amosISA <amosisa700@gmail.com>
2019-02-18 08:04:16 +0100

    Snippet for multiple images in museo app

--- museo/admin.py
+++ museo/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Classe, Tipus, Museo
+from .models import Classe, Tipus, Museo, Imatge
 
 # Register your models here.
+class ImatgeInline(admin.StackedInline):
+    model = Imatge
+    extra = 0
+
 class MuseoAdmin(admin.ModelAdmin):
     list_display = ('n_inventari', 'nom', 'classe', 'tipus', 'utilitat', 'propietari')
     list_filter = ('classe', 'tipus')
@@ -12,6 +16,14 @@ class MuseoAdmin(admin.ModelAdmin):
         'classe',
         'tipus',
     )
+    inlines = [ImatgeInline]
+
+    def save_model(self, request, obj, form, change):
+        super(MuseoAdmin, self).save_model(request, obj, form, change)
+        # obj.save()
+
+        for afile in request.FILES.getlist('photos_multiple'):
+            obj.imatges.create(imatge=afile)
 admin.site.register(Museo, MuseoAdmin)
 
 class ClasseAdmin(admin.ModelAdmin):

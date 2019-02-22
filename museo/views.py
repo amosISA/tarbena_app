bd30dd4c6806ee4beae75c485c28feb1a84ce4f6
amosISA <amosisa700@gmail.com>
2019-02-22 11:01:33 +0100

    Added detailed feature museum

--- museo/views.py
+++ museo/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required, permission_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Museo
 
 # Create your views here.
 #@login_required()
+# --------------- Museo Index --------------- #
 def index_museo(request):
     museos = Museo.objects.all().select_related(
         'tipus', 'classe'
@@ -13,3 +14,14 @@ def index_museo(request):
     return render(request,
                   'museo/index.html',
                   {'museos': museos})
+
+# --------------- Museo Details --------------- #
+def museo_detail(request, id):
+    museu = get_object_or_404(Museo.objects.select_related(
+                                            'tipus', 'classe'
+                                        ),
+                                   id=id)
+
+    return render(request,
+                  'museo/detail.html',
+                  {'museu': museu})

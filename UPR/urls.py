3b87484a3962d689a560f6ce97de41e8b18c2d30
amosISA <amosisa700@gmail.com>
2019-05-30 12:35:16 +0200

    Ajax create incidencia: UPR

--- UPR/urls.py
+++ UPR/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.index_maquinas, name='index'),
     url(r'^maquina/(?P<ninventario>[-\w\d]+)/$', views.maquina_detail, name='maquina_detail'),
     url(r'^incidencia/(?P<ninventario>[-\w\d]+)/$', views.add_incidencia, name='add_incidencia'),
+    url(r'^ajaxgetcomponentsbygroup/$', views.get_components_by_group, name='get_components_by_group'),
+    #url(r'^incidencia/new/$', views.NewMaquinaIncidencia.as_view(), name='create_maquina_incidencia'),
 ]
